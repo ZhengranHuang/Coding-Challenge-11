4474374bf436c3e878340845706943eb84d22b27
ZhengranHuang <Zhengran@usf.edu>
2024-10-25 19:23:26 -0400

    Handle order submission and display order summary

--- main.js
+++ main.js
@@ -13,4 +13,10 @@ totalPriceElement.textContent = totalPrice.toFixed(2);
 }
 productSelector.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice);
-
+//Task4:Handle Order Submission
+placeOrderButton.addEventListener('click', function() {
+const selectedProduct = productSelector.options[productSelector.selectedIndex].text;
+const quantity = quantityInput.value;
+const totalPriceSubmission = totalPriceElement.textContent;
+orderSummary.textContent = `You ordered ${quantity} of ${selectedProduct}. Total price: $${totalPriceSubmission}`;
+});
